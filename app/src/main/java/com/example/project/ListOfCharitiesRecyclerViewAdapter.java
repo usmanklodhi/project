0a1099d698dcd84e432adada23db0618cfaf47f9
Ramin Rafi <33839096+raminrafi@users.noreply.github.com>
2021-06-19 20:46:39 +0500

    Update ListOfCharitiesRecyclerViewAdapter.java

--- app/src/main/java/com/example/project/ListOfCharitiesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/project/ListOfCharitiesRecyclerViewAdapter.java
@@ -1,25 +1,48 @@
 package com.example.project;
 
 import android.content.Context;
-import android.database.Cursor;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListOfCharitiesRecyclerViewAdapter extends RecyclerView.Adapter<ListOfCharitiesRecyclerViewAdapter.ViewHolder> {
     Context context;
-    String s[];
-    public ListOfCharitiesRecyclerViewAdapter(Context ct, String s1[]) {
+    List<Charity> charityList;
+    List<Charity> charityListFull;
+    private Filter filter;
+
+    public ListOfCharitiesRecyclerViewAdapter(Context ct, List<Charity> charities) {
         context = ct;
-        s= s1;
+        charityList= charities;
+        charityListFull = new ArrayList<>(charityList);
     }
+
+    public void filterList(ArrayList<Charity> filteredList) {
+        charityList = filteredList;
+        notifyDataSetChanged();
+    }
+
+    ListOfCharitiesRecyclerViewAdapter(List <Charity> c){
+        this.charityList=c;
+        charityListFull = new ArrayList <>(c);
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -31,15 +54,52 @@ public class ListOfCharitiesRecyclerViewAdapter extends RecyclerView.Adapter<Lis
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // Set item views based on your views and data model
-        TextView textView = holder.CharityNameTextView;
-        textView.setText(s[position]);
+        //TextView textView = holder.CharityNameTextView;
+        //textView.setText(charityList.get(position).getCharityName());
 
+        Charity charity = charityList.get(position);
+        holder.CharityNameTextView.setText(charity.getCharityName());
+        //holder.title.setText(charity.getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return s.length;
+        return charityList.size();
     }
+
+   /* @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    charityListFiltered = charityListFull;
+                } else {
+                    List<Charity> filteredList = new ArrayList<>();
+                    for (Charity row : charityList) {
+                        if (row.getCharityName().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    charityListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = charityListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                charityListFiltered = (ArrayList<Charity>) filterResults.values;
+
+                // refresh the list with filtered data
+                notifyDataSetChanged();
+            }
+        };
+    }*/
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         // Your holder should contain a member variable
@@ -53,9 +113,15 @@ public class ListOfCharitiesRecyclerViewAdapter extends RecyclerView.Adapter<Lis
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
             super(itemView);
-
             CharityNameTextView = (TextView) itemView.findViewById(R.id.CharityNameOnRecyclerView);
             DonateButton = (Button) itemView.findViewById(R.id.DonateBtnOnRecyclerView);
+            DonateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    context.startActivity(new Intent(context, DonationActivity.class));
+
+                }
+            });
         }
     }
 
